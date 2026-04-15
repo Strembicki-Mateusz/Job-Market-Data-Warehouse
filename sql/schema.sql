4139e7db2c992ab675b9d38764f2dc9f2930ff27
Strembicki-Mateusz <117041712+Strembicki-Mateusz@users.noreply.github.com>
2026-04-15 19:21:37 +0200

    update schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,12 +2,16 @@ DROP TABLE IF EXISTS fact_jobs;
 DROP TABLE IF EXISTS dim_company;
 
 CREATE TABLE dim_company (
-    company_id INT PRIMARY KEY
+    company_id SERIAL PRIMARY KEY,
+    company_name TEXT UNIQUE
 );
 
 CREATE TABLE fact_jobs (
     job_id INT PRIMARY KEY,
     job_title TEXT,
     company_id INT,
+    salary TEXT,
+    location TEXT,
+    category TEXT,
     FOREIGN KEY (company_id) REFERENCES dim_company(company_id)
 );
